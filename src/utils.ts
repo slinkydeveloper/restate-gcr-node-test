c1906d1acbf2ee9593c30a299f23ea39d421058a
slinkydeveloper <me@slinkydeveloper.com>
2026-02-06 12:38:59 +0100

    Create a bug

--- src/utils.ts
+++ src/utils.ts
@@ -11,10 +11,10 @@ export function sendNotification(greetingId: string, name: string) {
 }
 
 export function sendReminder(greetingId: string, name: string) {
-  if (Math.random() < 0.7 && name == "Alice") {
+  // if (Math.random() < 0.7 && name == "Alice") {
     // 70% chance of failure
     console.error(`[👻 SIMULATED] Failed to send reminder: ${greetingId}`);
     throw new Error(`[👻 SIMULATED] Failed to send reminder: ${greetingId}`);
-  }
-  console.log(`Reminder sent: ${greetingId}`);
+  // }
+  // console.log(`Reminder sent: ${greetingId}`);
 }
